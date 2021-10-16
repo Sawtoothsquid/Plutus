70367ab54440afb0834b8bc02e76110d1b4838f2
Sawtoothsquid <92612945+Sawtoothsquid@users.noreply.github.com>
2021-10-15 21:25:12 -0400

    Delete testing directory

--- testing/electrum.py
+++
@@ -1,9 +0,0 @@
-#from bitcoinlib.mnemonic import Mnemonic
-import bitcoinlib
-
-words = bitcoinlib.mnemonic.Mnemonic().generate()
-key = bitcoinlib.keys.HDKey().from_passphrase(words)
-print(key)
-print(key.public())
-print(key.address())
-print(key.address_uncompressed())
